3ab00ddd049ac161ac54edf8c644c6c90505239d
Patrick Chavez <patrickchavez64@yahoo.ca>
2021-03-28 18:49:19 -0400

    Added trees to the overworld

---
+++ Assets/Scripts/DontDestroyOthers.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOthers : MonoBehaviour
+{
+    private static DontDestroyOthers original;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Code from Hellium
+    private void Awake()
+    {
+
+        if (original != this)
+        {
+            if (original != null)
+            {
+                Destroy(original.gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+            original = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+}
